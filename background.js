7f12e8f109858027312628da5f1e18cb6c5e405d
28oso <28osoblanco2020@gmail.com>
2024-09-12 22:36:44 +0900

    feat: enchange text

--- background.js
+++ background.js
@@ -44,7 +44,7 @@ function replaceAuxiliaryVerbsWithUho() {
     // // 接続助詞「て」＋（助動詞かつ基本形が「ます」→「て」を「とり」に置き換え
     // // 接続助詞「て」＋動詞「い」＋（助動詞かつ基本形が「ます」）→（「て」＋「い」）を「とり」に置き換え
 
-    // 〜でいます→〜どります
+    // 〜でいます→〜どります(済)
     // // 接続助詞「で」＋（助動詞かつ基本形が「ます」）→「で」を「どり」に置き換え
     // // 接続助詞「で」＋動詞「い」＋（助動詞かつ基本形が「ます」）→（「で」＋「い」）を「どり」に置き換え
 
@@ -93,12 +93,16 @@ function replaceAuxiliaryVerbsWithUho() {
         const tokens = tokenizer.tokenize(originalText);
 
         tokens.forEach(token => {
-          console.log(token);  
+          // console.log(token);  
         });
 
         var newtokens = [];
         for (let i = 0; i < tokens.length; i++) {
-          if (norn_first_asshi(tokens[i].surface_form)){
+          console.log(tokens[i]);
+          if (tokens[i] === undefined || tokens[i+1] === undefined){
+            newtokens.push(tokens[i].surface_form);
+            continue;
+          }else if (norn_first_asshi(tokens[i].surface_form)){
             // 一人称→あっし
             newtokens.push('あっし');
           }else if (norn_second_omee(tokens[i].surface_form)){
@@ -111,12 +115,60 @@ function replaceAuxiliaryVerbsWithUho() {
             newtokens.push('旦那');
             i += 1;
           }else if (tokens[i].surface_form === 'て' && tokens[i].pos_detail_1 === '接続助詞'){
-            // 〜ています→〜とります(済)
-            if (tokens[i+1].surface_form === 'い' && tokens[i+1].pos === '動詞'){
-              if (tokens[i+2].surface_form === 'ます' && tokens[i+2].pos === '助動詞'){
+            // 〜ています→〜とります
+            if (tokens[i+1].basic_form === 'ます' && tokens[i+1].pos === '助動詞'){
+              newtokens.push('と');
+              newtokens.push('り');
+              if (tokens[i+1].surface_form === 'ます' && tokens[i+1].pos === '助動詞'){
+                newtokens.push('やす');
+              } else {
+                newtokens.push(tokens[i+1].surface_form);
+              }
+              i += 1;
+            } else if (tokens[i+1].surface_form === 'い' && tokens[i+1].pos === '動詞'){
+              if (tokens[i+2].basic_form === 'ます' && tokens[i+2].pos === '助動詞'){
                 newtokens.push('と');
                 newtokens.push('り');
-                newtokens.push('ます');
+                if (tokens[i+2].surface_form === 'ます' && tokens[i+2].pos === '助動詞'){
+                  newtokens.push('やす');
+                } else {
+                  newtokens.push(tokens[i+2].surface_form);
+                }
+                i += 2;
+              } else {
+                newtokens.push(tokens[i].surface_form);
+                newtokens.push(tokens[i+1].surface_form);
+                i += 1;
+              }
+            }else if (tokens[i+1].surface_form === 'しまう' && tokens[i+1].pos === '動詞'){
+              // 〜てしまう、〜ちゃう→〜ちまう、〜やがる
+              // // 動詞かつ　基本形が「ちゃう」→「ちゃ」を「ちま」に置き換え
+              // // 助詞「て」＋（動詞かつ　基本形が「しまう」）→「しま」を「ちま」に置き換え
+              newtokens.push('ちまう');
+              i += 1;
+            }else{
+              newtokens.push(tokens[i].surface_form);
+            }
+          }else if (tokens[i].surface_form === 'で' && tokens[i].pos_detail_1 === '接続助詞'){
+            // 〜でいます→〜どります
+            if (tokens[i+1].basic_form === 'ます' && tokens[i+1].pos === '助動詞'){
+              newtokens.push('ど');
+              newtokens.push('り');
+              if (tokens[i+1].surface_form === 'ます' && tokens[i+1].pos === '助動詞'){
+                newtokens.push('やす');
+              } else {
+                newtokens.push(tokens[i+1].surface_form);
+              }
+              i += 1;
+            } else if (tokens[i+1].surface_form === 'い' && tokens[i+1].pos === '動詞'){
+              if (tokens[i+2].basic_form === 'ます' && tokens[i+2].pos === '助動詞'){
+                newtokens.push('ど');
+                newtokens.push('り');
+                if (tokens[i+2].surface_form === 'ます' && tokens[i+2].pos === '助動詞'){
+                  newtokens.push('やす');
+                } else {
+                  newtokens.push(tokens[i+2].surface_form);
+                }
                 i += 2;
               } else {
                 newtokens.push(tokens[i].surface_form);
@@ -126,37 +178,16 @@ function replaceAuxiliaryVerbsWithUho() {
             }else{
               newtokens.push(tokens[i].surface_form);
             }
-          // }else if (tokens[i].surface_form === 'で' && tokens[i].pos_detail_1 === '接続助詞'){
-          //   // 〜でいます→〜どります
-          //   // // 接続助詞「で」＋（助動詞かつ基本形が「ます」）→「で」を「どり」に置き換え
-          //   // // 接続助詞「で」＋動詞「い」＋（助動詞かつ基本形が「ます」）→（「で」＋「い」）を「どり」に置き換え
-          //   if (tokens[i+1].surface_form === 'い' && tokens[i+1].pos === '動詞'){
-          //     if (tokens[i+2].surface_form === 'ます' && tokens[i+2].pos === '助動詞'){
-          //       newtokens.push('ど');
-          //       newtokens.push('り');
-          //       newtokens.push('ます');
-          //       i += 2;
-          //     } else {
-          //       newtokens.push(tokens[i].surface_form);
-          //       newtokens.push(tokens[i+1].surface_form);
-          //       i += 1;
-          //     }
-          //   }else{
-          //     newtokens.push(tokens[i].surface_form);
-          //   }
-          // }else if (1) {
-          // 〜ます→〜やす
-          // // 助動詞かつ（基本形が「ます」）→「ま」を「や」に置き換え
-          // }else if (1) {
-          // 〜ない→〜ねぇ
-          // //（助動詞or形容詞）かつ（読み方が「ナイ」）→「ねぇ」に置き換え
-          // }else if (1) {
-          // 〜ください→〜くだせぇ
-          // // 動詞かつ読み方が「クダサイ」→「くだせぇ」に置き換え
-          // }else if (1) {
-          // 〜てしまう、〜ちゃう→〜ちまう、〜やがる
-          // // 動詞かつ　基本形が「ちゃう」→「ちゃ」を「ちま」に置き換え
-          // // 助詞「て」＋（動詞かつ　基本形が「しまう」）→「しま」を「ちま」に置き換え
+          }else if (tokens[i].surface_form === 'ます' && tokens[i].pos === '助動詞'){
+            // 〜ます→〜やす
+            newtokens.push('やす');
+          }else if ((tokens[i].surface_form === 'ない' || tokens[i].reading === 'ナイ') && (tokens[i].pos === '助動詞' || tokens[i].pos === '形容詞')){
+            // 〜ない→〜ねぇ
+            newtokens.push('ねぇ');
+          }else if (tokens[i].surface_form === 'ください' || tokens[i].reading === 'クダサイ'){
+            // 〜ください→〜くだせぇ
+            // // 動詞かつ読み方が「クダサイ」→「くだせぇ」に置き換え
+            newtokens.push('くだせぇ');
           }else{
             newtokens.push(tokens[i].surface_form);
           }
