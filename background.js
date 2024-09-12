5aa96534bd28f86fd8cdd65cb8934a43d05ba5a4
28oso <28osoblanco2020@gmail.com>
2024-09-12 17:42:36 +0900

    feat: second & third person

--- background.js
+++ background.js
@@ -33,12 +33,12 @@ function replaceAuxiliaryVerbsWithUho() {
     }
     // 二人称→おめえ、おめえさん、てめえ
     function norn_second_omee(surface_form){
-      const norn_second = ['あなた', 'きみ', 'おまえ', 'アナタ', 'キミ', 'オマエ'];
+      const norn_second = ['君', 'あなた', 'きみ', 'おまえ', 'アナタ', 'キミ', 'オマエ'];
       if (norn_second.includes(surface_form)) {
         return 1;
       }
     }
-    // 三人称→◯◯の旦那
+    // 三人称→◯◯の旦那(済)
 
     // 〜ています→〜とります(済)
     // // 接続助詞「て」＋（助動詞かつ基本形が「ます」→「て」を「とり」に置き換え
@@ -101,10 +101,19 @@ function replaceAuxiliaryVerbsWithUho() {
           if (norn_first_asshi(tokens[i].surface_form)){
             // 一人称→あっし
             newtokens.push('あっし');
+          }else if (norn_second_omee(tokens[i].surface_form)){
+            // 二人称→おめえ、おめえさん、てめえ
+            newtokens.push('おめえ');
+          }else if (tokens[i].pos_detail_3 === "姓" && tokens[i+1].pos_detail_1 === "接尾"){
+            // 三人称→◯◯の旦那
+            newtokens.push(tokens[i].surface_form);
+            newtokens.push('の');
+            newtokens.push('旦那');
+            i += 1;
           }else if (tokens[i].surface_form === 'て' && tokens[i].pos_detail_1 === '接続助詞'){
+            // 〜ています→〜とります(済)
             if (tokens[i+1].surface_form === 'い' && tokens[i+1].pos === '動詞'){
               if (tokens[i+2].surface_form === 'ます' && tokens[i+2].pos === '助動詞'){
-                // 〜ています→〜とります(済)
                 newtokens.push('と');
                 newtokens.push('り');
                 newtokens.push('ます');
@@ -117,6 +126,37 @@ function replaceAuxiliaryVerbsWithUho() {
             }else{
               newtokens.push(tokens[i].surface_form);
             }
+          // }else if (tokens[i].surface_form === 'で' && tokens[i].pos_detail_1 === '接続助詞'){
+          //   // 〜でいます→〜どります
+          //   // // 接続助詞「で」＋（助動詞かつ基本形が「ます」）→「で」を「どり」に置き換え
+          //   // // 接続助詞「で」＋動詞「い」＋（助動詞かつ基本形が「ます」）→（「で」＋「い」）を「どり」に置き換え
+          //   if (tokens[i+1].surface_form === 'い' && tokens[i+1].pos === '動詞'){
+          //     if (tokens[i+2].surface_form === 'ます' && tokens[i+2].pos === '助動詞'){
+          //       newtokens.push('ど');
+          //       newtokens.push('り');
+          //       newtokens.push('ます');
+          //       i += 2;
+          //     } else {
+          //       newtokens.push(tokens[i].surface_form);
+          //       newtokens.push(tokens[i+1].surface_form);
+          //       i += 1;
+          //     }
+          //   }else{
+          //     newtokens.push(tokens[i].surface_form);
+          //   }
+          // }else if (1) {
+          // 〜ます→〜やす
+          // // 助動詞かつ（基本形が「ます」）→「ま」を「や」に置き換え
+          // }else if (1) {
+          // 〜ない→〜ねぇ
+          // //（助動詞or形容詞）かつ（読み方が「ナイ」）→「ねぇ」に置き換え
+          // }else if (1) {
+          // 〜ください→〜くだせぇ
+          // // 動詞かつ読み方が「クダサイ」→「くだせぇ」に置き換え
+          // }else if (1) {
+          // 〜てしまう、〜ちゃう→〜ちまう、〜やがる
+          // // 動詞かつ　基本形が「ちゃう」→「ちゃ」を「ちま」に置き換え
+          // // 助詞「て」＋（動詞かつ　基本形が「しまう」）→「しま」を「ちま」に置き換え
           }else{
             newtokens.push(tokens[i].surface_form);
           }
