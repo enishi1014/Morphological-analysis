a371a59b032f1d5a869df32937428d3adb13e613
marugame <futajapan2000@gmail.com>
2024-09-14 14:28:21 +0900

    Content.js content mixed with background.js

--- background.js
+++ background.js
@@ -22,6 +22,32 @@ function replaceAuxiliaryVerbsWithUho() {
 
     const textNodes = getTextNodesIn(document.body);
 
+    const fontUrl = chrome.runtime.getURL("fonts/KouzanBrushFont.ttf");
+
+    const fontFace = `
+    @font-face {
+      font-family: '衡山毛筆フォント';
+      src: url('${fontUrl}') format('truetype');
+    }
+    `;
+
+    const styleContent = `
+    ${fontFace}
+    body, body * {
+      font-family: '衡山毛筆フォント' !important;
+      text-shadow:
+        0.2px 0 0 black,
+        -0.2px 0 0 black,
+        0 0.2px 0 black,
+        0 -0.2px 0 black;
+    }
+    }
+    `;
+
+  const styleElement = document.createElement('style');
+  styleElement.textContent = styleContent;
+  document.head.appendChild(styleElement);
+
     // 辞書
     // 一人称→あっし
     function norn_first_asshi(surface_form) {
